279d588e0a03ee29ec8a16211a1f71148a98c850
Desktop <imbxyz1@gmail.com>
2023-02-04 09:13:47 -0800

    STL strings vs C-strings

--- arrays.cpp
+++ arrays.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main(){
@@ -12,13 +14,29 @@ int main(){
     int a[] = {10, 11, 12, 13}; // declares and initializes a[4]
     bool b[] = {false, true};  // declares and initializes b[2]
     char c[] = {'c', 'a', 't'}; // declares and initializes c[3]
+    char d[] = {'c','a','t'};
     char* p = c;
     char* q = &c[0];
+    if (c == p){ // compares the address of the first element
+        cout << "equal" << endl;
+    } else{
+        cout << "false" << endl;
+    }
+    cout << "c location: " << c[2] << endl;
+    cout << "p pointer: " << p[2] << endl << "q pointer: " << q[2] << endl;
 
-    cout << "c location: " << &c << endl;
-    cout << "p pointer: " << *p << endl << "q pointer: " << q[2] << endl;
+    string s = "to be";
+    string t = "not " + s;
+    string u = s + " or " + t;
+    if (s > t)
+        cout << u << endl;
 
-
+    string john = "John";
+    int johnSize = john.size();
+    char js = john[3];
+    john += " Smith";
+    johnSize = john.size();
+    cout << john << endl << johnSize << endl;
 
     return EXIT_SUCCESS;
 }
