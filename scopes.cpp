a90c3306c9d99b0d3744092dd95c6ebe9e82ae1c
MacBook Pro <imbxyz1@gmail.com>
2023-02-09 12:10:49 -0800

    expressions.cpp

--- scopes.cpp
+++ scopes.cpp
@@ -1,11 +1,22 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-using namespace std;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+namespace myglobals{
+    int cat;
+    string dog = "bow wow";
+}
+using namespace myglobals;
+
+
 
 const int Cat = 1;
 
 int main(){
+    cout << myglobals::dog << endl;
     const double PI = 3.14159265;
     const int CUT_OFF[] = {90, 80, 70, 60};
     const int N_DAYS = 7;
@@ -16,7 +27,7 @@ int main(){
     typedef double Coordinate;
     BufferPtr p;
     Coordinate x,y;
-
+    cout << sizeof(BufferPtr) << endl;
     const int Cat = 2;
     cout << Cat << endl;
 }
