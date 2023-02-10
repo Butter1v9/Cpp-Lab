a4cdb3153f3eb0affba9415bb10c03b4fa60530b
MacBook Pro <imbxyz1@gmail.com>
2023-02-09 19:38:30 -0800

    compiling using g++

--- casting.cpp
+++ casting.cpp
@@ -11,7 +11,15 @@ int main(){
 
     cout << z << endl;
     cout << x << endl;
-    
+
+    // static casting, does not round, but rather truncate
+    double d1 = 3.2;
+    double d2 = 3.99999;
+    int i1 = static_cast<int>(d1); // 3
+    int i2 = static_cast<int>(d2); // 3
+
+    cout << i1 << endl << i2 << endl;
+
 
 
     return EXIT_SUCCESS;
