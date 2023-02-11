8a6db11d10c0a2dba13c0b866e07600b569fa5a4
Desktop <imbxyz1@gmail.com>
2023-02-11 05:11:20 -0800

    idk

---
+++ STL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    vector <int> scores(100);
+    vector <char> buffer(500);
+    
+
+    return EXIT_SUCCESS;
+}
