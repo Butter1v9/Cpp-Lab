4972c2cea0a37b1fa7d22e8456f7d35c7a9d487b
MacBook Pro <imbxyz1@gmail.com>
2023-02-11 01:59:32 -0800

    idk

--- classes.cpp
+++ classes.cpp
@@ -31,6 +31,19 @@ class Passenger{
 
 };
 
+class Vect{
+    public:
+        Vect();
+        Vect(int n);
+        ~Vect();
+        int getSize();
+    private:
+        int size;
+        int* data;
+
+
+};
+
 int main(){
     Counter ctr;
     cout << ctr.getCount() << endl;
@@ -47,6 +60,9 @@ int main(){
     cout << copy.getName() << endl;
     cout << copyCat.getName() << endl;
 
+    Vect *p1 = new Vect(4);
+    delete p1;
+
     return 0;
 
 
@@ -57,12 +73,9 @@ Counter::Counter(){count = 0;}
 int Counter::getCount(){return count;}
 void Counter::increaseBy(int n) {count += n;}
 
-Passenger::Passenger(){
-    name = "--NO NAME--";
-    mealPref = NO_PREF;
-    isFreqFlyer = false;
-    freqFlyerNum = "NONE";
-}
+Passenger::Passenger()
+    :name("--NO NAME--"), mealPref(NO_PREF), isFreqFlyer(false), freqFlyerNum("NONE") // initializing class members with initializer lists
+    {}
 Passenger::Passenger(const string& nm, MealType mp, const string& ffn){
     name = nm;
     mealPref = mp;
@@ -79,3 +92,12 @@ Passenger::Passenger(const Passenger& pass){
 string Passenger::getName(){
     return name;
 }
+
+Vect::Vect(int n)
+    :size(n),data(new int[n])
+    {cout << getSize() << endl; }
+Vect::~Vect(){
+    cout << "destructor activated" << endl;
+    delete [] data;
+}
+int Vect::getSize(){return data[0];}
